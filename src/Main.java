2a3ab47f2b31f90f19178954241229776650154c
mlangefeld <langefmn@mail.uc.edu>
2023-10-05 16:13:54 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        String d = "D";
+        String r = "R";
+        String i = "I";
+        String o = "O";
+
+        System.out.print("Which party are you affiliated with? [D,R,I,O] ");
+
+        if ()
+
+    }
+}
