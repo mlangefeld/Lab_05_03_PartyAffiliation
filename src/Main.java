6cb913ee12f1529d5c246852e2948e0ac8e9a407
mlangefeld <langefmn@mail.uc.edu>
2023-10-11 15:55:13 -0400

    Final commit

--- src/Main.java
+++ src/Main.java
@@ -2,16 +2,21 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
 
-        String d = "D";
-        String r = "R";
-        String i = "I";
-        String o = "O";
+        System.out.print("Which party are you affiliated with? [D, R, I, or other]: ");
 
-        System.out.print("Which party are you affiliated with? [D,R,I,O] ");
+        char partyAfil = scanner.next().charAt(0);
 
-        if ()
+        if (partyAfil == 'D' || partyAfil == 'd'){
+            System.out.println("You're a Democrat, you get a donkey!");
+        } else if (partyAfil == 'R' || partyAfil == 'r') {
+            System.out.println("You're a Republican, you get an elephant!");
+        } else if (partyAfil == 'I' || partyAfil == 'i') {
+            System.out.println("You're an Independent, you get a random person!");
+        } else {
+            System.out.println("You chose other, here's a gold star!");
+        }
 
     }
 }
